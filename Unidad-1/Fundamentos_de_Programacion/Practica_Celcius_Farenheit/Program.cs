b69b3bea290467a192a6cff8e66a6835fc42a0b5
LuisTZR <luisangeltzr@gmail.com>
2025-09-03 11:31:26 -0500

    Add files via upload

---
+++ Unidad-1/Fundamentos_de_Programacion/Practica_Celcius_Farenheit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Celcius_Farenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dblValorCelcius = 0;
+            double dblValorFarenheit = 0;
+            Console.WriteLine("Ingrese un valor de grados en Celcius para convertir a grados Fareheit:");
+            dblValorCelcius = double.Parse(Console.ReadLine());
+            dblValorFarenheit = (dblValorCelcius * 1.8) + 32 ;
+            Console.WriteLine($"El valor de {dblValorCelcius} grados Celcius tiene un valor de {dblValorFarenheit} grados en Farenheit.");
+        }
+    }
+}
