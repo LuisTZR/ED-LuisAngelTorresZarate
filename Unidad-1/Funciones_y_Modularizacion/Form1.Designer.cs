3a8c40f274e73de4f7ef383a7f4bce96c8a602e9
LuisTZR <luisangeltzr@gmail.com>
2025-09-04 20:39:40 -0500

    Agrega practica calculadora y lista

---
+++ Unidad-1/Funciones_y_Modularizacion/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Funciones_y_Modularizacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpCalculadora = new System.Windows.Forms.GroupBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.grpPromedio = new System.Windows.Forms.GroupBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.btnCalcularPromedio = new System.Windows.Forms.Button();
+            this.btnLimpiarLista = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPromedio = new System.Windows.Forms.Label();
+            this.grpCalculadora.SuspendLayout();
+            this.grpPromedio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpCalculadora
+            // 
+            this.grpCalculadora.Controls.Add(this.txtB);
+            this.grpCalculadora.Controls.Add(this.txtA);
+            this.grpCalculadora.Controls.Add(this.lblB);
+            this.grpCalculadora.Controls.Add(this.lblA);
+            this.grpCalculadora.Controls.Add(this.lblResultado);
+            this.grpCalculadora.Controls.Add(this.labelResultado);
+            this.grpCalculadora.Controls.Add(this.btnDividir);
+            this.grpCalculadora.Controls.Add(this.btnRestar);
+            this.grpCalculadora.Controls.Add(this.btnMultiplicar);
+            this.grpCalculadora.Controls.Add(this.btnSumar);
+            this.grpCalculadora.Location = new System.Drawing.Point(12, 12);
+            this.grpCalculadora.Name = "grpCalculadora";
+            this.grpCalculadora.Size = new System.Drawing.Size(230, 219);
+            this.grpCalculadora.TabIndex = 0;
+            this.grpCalculadora.TabStop = false;
+            this.grpCalculadora.Text = "Calculadora";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(40, 68);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(60, 23);
+            this.btnSumar.TabIndex = 0;
+            this.btnSumar.Text = "+";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(40, 106);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(60, 23);
+            this.btnMultiplicar.TabIndex = 1;
+            this.btnMultiplicar.Text = "x";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(142, 68);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(60, 23);
+            this.btnRestar.TabIndex = 2;
+            this.btnRestar.Text = "-";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(142, 106);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(60, 23);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "÷";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(16, 163);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(58, 13);
+            this.labelResultado.TabIndex = 4;
+            this.labelResultado.Text = "Resultado:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(72, 163);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(16, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "...";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(17, 27);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(17, 13);
+            this.lblA.TabIndex = 6;
+            this.lblA.Text = "A:";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(121, 27);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(17, 13);
+            this.lblB.TabIndex = 7;
+            this.lblB.Text = "B:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(40, 24);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(75, 20);
+            this.txtA.TabIndex = 8;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(142, 24);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(76, 20);
+            this.txtB.TabIndex = 9;
+            // 
+            // grpPromedio
+            // 
+            this.grpPromedio.Controls.Add(this.btnLimpiarLista);
+            this.grpPromedio.Controls.Add(this.lblPromedio);
+            this.grpPromedio.Controls.Add(this.btnCalcularPromedio);
+            this.grpPromedio.Controls.Add(this.label1);
+            this.grpPromedio.Controls.Add(this.lstNumeros);
+            this.grpPromedio.Controls.Add(this.btnAgregar);
+            this.grpPromedio.Controls.Add(this.txtNumero);
+            this.grpPromedio.Controls.Add(this.lblNumero);
+            this.grpPromedio.Location = new System.Drawing.Point(248, 12);
+            this.grpPromedio.Name = "grpPromedio";
+            this.grpPromedio.Size = new System.Drawing.Size(281, 219);
+            this.grpPromedio.TabIndex = 1;
+            this.grpPromedio.TabStop = false;
+            this.grpPromedio.Text = "Promedio";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(6, 24);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(47, 13);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Numero:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(59, 24);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(168, 22);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(58, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.Location = new System.Drawing.Point(9, 50);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(150, 121);
+            this.lstNumeros.TabIndex = 3;
+            // 
+            // btnCalcularPromedio
+            // 
+            this.btnCalcularPromedio.Location = new System.Drawing.Point(165, 106);
+            this.btnCalcularPromedio.Name = "btnCalcularPromedio";
+            this.btnCalcularPromedio.Size = new System.Drawing.Size(110, 23);
+            this.btnCalcularPromedio.TabIndex = 4;
+            this.btnCalcularPromedio.Text = "Calcular promedio";
+            this.btnCalcularPromedio.UseVisualStyleBackColor = true;
+            this.btnCalcularPromedio.Click += new System.EventHandler(this.btnCalcularPromedio_Click);
+            // 
+            // btnLimpiarLista
+            // 
+            this.btnLimpiarLista.Location = new System.Drawing.Point(168, 148);
+            this.btnLimpiarLista.Name = "btnLimpiarLista";
+            this.btnLimpiarLista.Size = new System.Drawing.Size(107, 23);
+            this.btnLimpiarLista.TabIndex = 5;
+            this.btnLimpiarLista.Text = "Limpiar lista";
+            this.btnLimpiarLista.UseVisualStyleBackColor = true;
+            this.btnLimpiarLista.Click += new System.EventHandler(this.btnLimpiarLista_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Promedio:";
+            // 
+            // lblPromedio
+            // 
+            this.lblPromedio.AutoSize = true;
+            this.lblPromedio.Location = new System.Drawing.Point(66, 183);
+            this.lblPromedio.Name = "lblPromedio";
+            this.lblPromedio.Size = new System.Drawing.Size(16, 13);
+            this.lblPromedio.TabIndex = 5;
+            this.lblPromedio.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 263);
+            this.Controls.Add(this.grpPromedio);
+            this.Controls.Add(this.grpCalculadora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpCalculadora.ResumeLayout(false);
+            this.grpCalculadora.PerformLayout();
+            this.grpPromedio.ResumeLayout(false);
+            this.grpPromedio.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpCalculadora;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.GroupBox grpPromedio;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.Button btnCalcularPromedio;
+        private System.Windows.Forms.Button btnLimpiarLista;
+        private System.Windows.Forms.Label lblPromedio;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Unidad-1/Funciones_y_Modularizacion/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Funciones_y_Modularizacion
+{
+    public partial class Form1 : Form
+    {
+        private List<double> numeros = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double Sumar(double a, double b) => a + b;
+        private double Restar(double a, double b) => a - b;
+        private double Multiplicar(double a, double b) => a * b;
+        private double Dividir(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException("No se puede dividir entre cero.");
+            return a / b;
+        }
+
+        // Función que recibe la lista y devuelve su promedio
+        private double Promedio(List<double> lista)
+        {
+            if (lista == null || lista.Count == 0) throw new InvalidOperationException("La lista está vacía.");
+            double suma = 0;
+            foreach (var n in lista) suma += n;
+            return suma / lista.Count;
+        }
+
+        private bool TryParseDouble(string texto, out double valor)
+        {
+            if (double.TryParse(texto, out valor))
+                return true;
+
+            MessageBox.Show("Introduce un número válido.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return false;
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            if (!TryParseDouble(txtA.Text, out double a)) return;
+            if (!TryParseDouble(txtB.Text, out double b)) return;
+
+            double r = Sumar(a, b);
+            lblResultado.Text = r.ToString();
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            if (!TryParseDouble(txtA.Text, out double a)) return;
+            if (!TryParseDouble(txtB.Text, out double b)) return;
+
+            double r = Restar(a, b);
+            lblResultado.Text = r.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            if (!TryParseDouble(txtA.Text, out double a)) return;
+            if (!TryParseDouble(txtB.Text, out double b)) return;
+
+            double r = Multiplicar(a, b);
+            lblResultado.Text = r.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            if (!TryParseDouble(txtA.Text, out double a)) return;
+            if (!TryParseDouble(txtB.Text, out double b)) return;
+
+            try
+            {
+                double r = Dividir(a, b);
+                lblResultado.Text = r.ToString();
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lblResultado.Text = "∞";
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!TryParseDouble(txtNumero.Text, out double n)) return;
+
+            numeros.Add(n);
+            lstNumeros.Items.Add(n.ToString());
+            txtNumero.Clear();
+            txtNumero.Focus();
+        }
+
+        private void btnCalcularPromedio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double p = Promedio(numeros);
+                lblPromedio.Text = p.ToString("G"); // formato general
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message, "Lista vacía", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnLimpiarLista_Click(object sender, EventArgs e)
+        {
+            numeros.Clear();
+            lstNumeros.Items.Clear();
+            lblPromedio.Text = string.Empty;
+            txtNumero.Clear();
+        }
+    }
+}
